9af47e2b330e9cd1a581cd258e0dee76646589aa
djyos <lst2012@yeah.net>
2019-01-05 18:35:59 +0800

    漏提交了这个文件

--- bsp/chipdrv/lan8720/lan8720.c
+++ bsp/chipdrv/lan8720/lan8720.c
@@ -38,7 +38,8 @@
 //%$#@target = header           //header = 生成头文件,cmdline = 命令行变量，DJYOS自有模块禁用
 //#ifndef CFG_VLAN_VALUE0         //****检查参数是否已经配置好
 //#warning    lan8720组件参数未配置，使用默认值
-//%$#@num,0,100,
+//%$#@num,0,255,
+#define CFG_LAN8720_PHY_ADDRESS     0   //phy的物理地址，硬件一般设计为 0
 //%$#@enum,true,false,
 //%$#@string,1,10,
 //%$#select,        ***定义无值的宏，仅用于第三方组件
@@ -47,54 +48,80 @@
 //%$#@end configue  ****参数配置结束
 //@#$%component end configure
 
-#define LAN8720_PHY_ADDRESS             0x00
+#define CN_RESET_DELAY      255000      //uS
+static bool_t sg_NeedMacDriver = false;
+__attribute__((weak)) u8 GMAC_MdioR(u8 dev, u8 reg, u16 *value)
+{
+    sg_NeedMacDriver = true;
+    return 0;
+}
 
-extern u8 GMAC_MdioR(u8 dev,u8 reg, u16 *value);
-extern u8 GMAC_MdioW(u8 dev,u8 reg, u16 value);
+__attribute__((weak)) u8 GMAC_MdioW(u8 dev,u8 reg, u16 value)
+{
+    sg_NeedMacDriver = true;
+    return 0;
+}
 extern uint32_t HAL_GetTick(void);
 bool_t lan8720Init(void)
 {
     u16   phyreg = 0;
-    uint32_t tickstart = 0;
-    uint32_t tickNow ;
+    u32 delay = 0;
+//  uint32_t tickstart = 0;
+//  uint32_t tickNow ;
     /*-------------------- PHY initialization and configuration ----------------*/
     /* Put the PHY in reset mode */
-    if((GMAC_MdioW(LAN8720_PHY_ADDRESS, PHY_BCR, PHY_RESET)) != 1)
+    if((GMAC_MdioW(CFG_LAN8720_PHY_ADDRESS, PHY_BCR, PHY_RESET)) != 1)
     {
-      /* Return false */
+        if(sg_NeedMacDriver == true)
+            printf("\r\n LAN8720驱动需要mac驱动");
+        return false;
+    }
+     Djy_EventDelay(CN_RESET_DELAY*mS);
+//   tickstart=HAL_GetTick();
+    do
+    {
+        GMAC_MdioR(CFG_LAN8720_PHY_ADDRESS, PHY_BSR, &phyreg);
+        if(delay > 1000)
+            return false;
+        else
+        {
+            Djy_DelayUs(1000);
+            delay++;
+        }
+//      tickNow=HAL_GetTick();
+//      if((tickNow - tickstart ) > 1000)
+//          return false;
+
+    } while (((phyreg & PHY_LINKED_STATUS) != PHY_LINKED_STATUS));
+    /* Enable Auto-Negotiation */
+    if((GMAC_MdioW(CFG_LAN8720_PHY_ADDRESS, PHY_BCR, PHY_AUTONEGOTIATION)) != 1)
+    {
       return false;
     }
-     Djy_EventDelay(PHY_RESET_DELAY*mS);
-     tickstart=HAL_GetTick();
-      do
-      {
-          GMAC_MdioR(LAN8720_PHY_ADDRESS, PHY_BSR, &phyreg);
-          tickNow=HAL_GetTick();
-          if((tickNow - tickstart ) > 1000)
-              return false;
+    /* Get tick */
+//  tickstart = HAL_GetTick();
+    delay = 0;
+    /* Wait until the auto-negotiation will be completed */
+    do
+    {
+        GMAC_MdioR(CFG_LAN8720_PHY_ADDRESS, PHY_BSR, &phyreg);
+        if(delay > 1000)
+            return false;
+        else
+        {
+            Djy_DelayUs(1000);
+            delay++;
+        }
+//      tickNow=HAL_GetTick();
+//      if((tickNow - tickstart ) > 10000)
+//          return false;
+    } while (((phyreg & PHY_AUTONEGO_COMPLETE) != PHY_AUTONEGO_COMPLETE));
 
-      } while (((phyreg & PHY_LINKED_STATUS) != PHY_LINKED_STATUS));
-      /* Enable Auto-Negotiation */
-      if((GMAC_MdioW(LAN8720_PHY_ADDRESS, PHY_BCR, PHY_AUTONEGOTIATION)) != 1)
-      {
+    /* Read the result of the auto-negotiation */
+    if((GMAC_MdioR(CFG_LAN8720_PHY_ADDRESS, PHY_SR, &phyreg)) != 1)
+    {
         return false;
-      }
-      /* Get tick */
-      tickstart = HAL_GetTick();
-      /* Wait until the auto-negotiation will be completed */
-      do
-      {
-          GMAC_MdioR(LAN8720_PHY_ADDRESS, PHY_BSR, &phyreg);
-          tickNow=HAL_GetTick();
-          if((tickNow - tickstart ) > 10000)
-              return false;
-      } while (((phyreg & PHY_AUTONEGO_COMPLETE) != PHY_AUTONEGO_COMPLETE));
-
-      /* Read the result of the auto-negotiation */
-      if((GMAC_MdioR(LAN8720_PHY_ADDRESS, PHY_SR, &phyreg)) != 1)
-      {
-        return false;
-      }
+    }
     return true;
 }
 
