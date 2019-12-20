297a4c895eab5ae92ddad9f389530d8a06d31180
Steven <934644852@qq.com>
2019-12-20 21:07:59 +0800

    1、修改配置文件笔误；
    Signed-off-by: Steven <934644852@qq.com>

--- component/djyfs/efs/efs.c
+++ component/djyfs/efs/efs.c
@@ -67,7 +67,7 @@
 //@#$%component configure   ****组件配置开始，用于 DIDE 中图形化配置界面
 //****配置块的语法和使用方法，参见源码根目录下的文件：component_config_readme.txt****
 //%$#@initcode      ****初始化代码开始，由 DIDE 删除“//”后copy到初始化文件中
-//    extern s32 ModuleInstall_EFS(const char *target, u32 opt, void *config);
+//    extern s32 ModuleInstall_EFS(const char *target, u32 opt, u32 config);
 //    ModuleInstall_EFS(CFG_EFS_MOUNT_POINT, CFG_EFS_INSTALL_OPTION, NULL);
 //%$#@end initcode  ****初始化代码结束
 
