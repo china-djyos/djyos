9489787142ce58e2f87b6671d1ea9cfcc1f82dbb
ck <523020590@qq.com>
2020-01-03 10:59:12 +0800

    修改了文件系统中，几个文件的编码格式，从UTF8换成了GBK

--- component/djyfs/filesystems.h
+++ component/djyfs/filesystems.h
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Copyright (c) 2018, Djyos Open source Development team. All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without
@@ -24,27 +24,27 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //-----------------------------------------------------------------------------
-// Copyright (c) 2018，著作权由都江堰操作系统开源开发团队所有。著作权人保留一切权利。
+// Copyright (c) 2018������Ȩ�ɶ����߲���ϵͳ��Դ�����Ŷ����С�����Ȩ�˱���һ��Ȩ����
 //
-// 这份授权条款，在使用者符合以下三条件的情形下，授予使用者使用及再散播本
-// 软件包装原始码及二进位可执行形式的权利，无论此包装是否经改作皆然：
+// �����Ȩ�����ʹ���߷��������������������£�����ʹ����ʹ�ü���ɢ����
+// ������װԭʼ�뼰����λ��ִ����ʽ��Ȩ�������۴˰�װ�Ƿ񾭸�����Ȼ��
 //
-// 1. 对于本软件源代码的再散播，必须保留上述的版权宣告、本条件列表，以
-//    及下述的免责声明。
-// 2. 对于本套件二进位可执行形式的再散播，必须连带以文件以及／或者其他附
-//    于散播包装中的媒介方式，重制上述之版权宣告、本条件列表，以及下述
-//    的免责声明。
-// 3. 本软件作为都江堰操作系统的组成部分，未获事前取得的书面许可，不允许移植到非
-//    都江堰操作系统环境下运行。
+// 1. ���ڱ�����Դ�������ɢ�������뱣�������İ�Ȩ���桢�������б�����
+//    ������������������
+// 2. ���ڱ��׼�����λ��ִ����ʽ����ɢ���������������ļ��Լ�������������
+//    ��ɢ����װ�е�ý�鷽ʽ����������֮��Ȩ���桢�������б����Լ�����
+//    ������������
+// 3. ��������Ϊ�����߲���ϵͳ����ɲ��֣�δ����ǰȡ�õ��������ɣ���������ֲ����
+//    �����߲���ϵͳ���������С�
 
-// 免责声明：本软件是本软件版权持有人以及贡献者以现状（"as is"）提供，
-// 本软件包装不负任何明示或默示之担保责任，包括但不限于就适售性以及特定目
-// 的的适用性为默示性担保。版权持有人及本软件之贡献者，无论任何条件、
-// 无论成因或任何责任主义、无论此责任为因合约关系、无过失责任主义或因非违
-// 约之侵权（包括过失或其他原因等）而起，对于任何因使用本软件包装所产生的
-// 任何直接性、间接性、偶发性、特殊性、惩罚性或任何结果的损害（包括但不限
-// 于替代商品或劳务之购用、使用损失、资料损失、利益损失、业务中断等等），
-// 不负任何责任，即在该种使用已获事前告知可能会造成此类损害的情形下亦然。
+// �����������������Ǳ�������Ȩ�������Լ�����������״��"as is"���ṩ��
+// ��������װ�����κ���ʾ��Ĭʾ֮�������Σ������������ھ��������Լ��ض�Ŀ
+// �ĵ�������ΪĬʾ�Ե�������Ȩ�����˼�������֮�����ߣ������κ�������
+// ���۳�����κ��������塢���۴�����Ϊ���Լ��ϵ���޹�ʧ������������Υ
+// Լ֮��Ȩ��������ʧ������ԭ��ȣ����𣬶����κ���ʹ�ñ�������װ��������
+// �κ�ֱ���ԡ�����ԡ�ż���ԡ������ԡ��ͷ��Ի��κν�����𺦣�����������
+// �������Ʒ������֮���á�ʹ����ʧ��������ʧ��������ʧ��ҵ���жϵȵȣ���
+// �����κ����Σ����ڸ���ʹ���ѻ���ǰ��֪���ܻ���ɴ����𺦵���������Ȼ��
 //-----------------------------------------------------------------------------
 
 #ifndef __FILE_SYSTEMS_H__
@@ -62,7 +62,7 @@ extern "C" {
 /*
  * These are the fs-independent mount-flags: up to 32 flags are supported
  */
-//定义来自Linux，并没有全实现
+//��������Linux����û��ȫʵ��
 #define MS_RDONLY       1  /* Mount read-only */
 #define MS_NOSUID       2  /* Ignore suid and sgid bits */
 #define MS_NODEV        4  /* Disallow access to device special files */
@@ -89,10 +89,10 @@ extern "C" {
 #define MS_I_VERSION    (1<<23) /* Update inode I_version field */
 #define MS_ACTIVE       (1<<30)
 #define MS_NOUSER       (1<<31)
-#define MS_DIRECTMOUNT  (1<<29) //DJYOS增加：直接mount到挂载点，不备份
-#define MS_INSTALLFORMAT  256   //DJYOS增加： 安装时会格式化整个文件系统
-#define MS_INSTALLUSE     512   //DJYOS增加：使用时才发生安装动作
-#define MS_INSTALLCREAT (1<<24) //DJYOS增加：文件系统不存在时，则新建
+#define MS_DIRECTMOUNT  (1<<29) //DJYOS���ӣ�ֱ��mount�����ص㣬������
+#define MS_INSTALLFORMAT  256   //DJYOS���ӣ� ��װʱ���ʽ�������ļ�ϵͳ
+#define MS_INSTALLUSE     512   //DJYOS���ӣ�ʹ��ʱ�ŷ�����װ����
+#define MS_INSTALLCREAT (1<<24) //DJYOS���ӣ��ļ�ϵͳ������ʱ�����½�
 
 struct FsCore;
 
@@ -109,17 +109,17 @@ struct filesystem{
 
 struct FsCore
 {
-    struct Object *pTarget;        // 挂载点
-    void *MediaInfo;          // 文件系统设备信息
-    void *MediaDrv;           // 文件系统设备驱动
-    void *pCore;                // 具体的文件系统控制信息
-    struct Object *MountBak;       // 用于隐藏原目录内容，如果 flags 带 MS_DIRECTMOUNT
-                                // 参数，则置空
-    struct filesystem *pFsType; // 所属文件系统类型
-    u32 InstallWay;             // 文件系统的安装方式，如MS_INSTALLCREAT；
-    void *Config;               // 文件系统的私有配置
-    s64 MediaStart;             // 在媒体中的起始unit,unit为单位；
-    s64 AreaSize;               // 所在区域的总大小；Byte为单位；
+    struct Object *pTarget;        // ���ص�
+    void *MediaInfo;          // �ļ�ϵͳ�豸��Ϣ
+    void *MediaDrv;           // �ļ�ϵͳ�豸����
+    void *pCore;                // ������ļ�ϵͳ������Ϣ
+    struct Object *MountBak;       // ��������ԭĿ¼���ݣ���� flags �� MS_DIRECTMOUNT
+                                // ���������ÿ�
+    struct filesystem *pFsType; // �����ļ�ϵͳ����
+    u32 InstallWay;             // �ļ�ϵͳ�İ�װ��ʽ����MS_INSTALLCREAT��
+    void *Config;               // �ļ�ϵͳ��˽������
+    s64 MediaStart;             // ��ý���е���ʼunit,unitΪ��λ��
+    s64 AreaSize;               // ����������ܴ�С��ByteΪ��λ��
 };
 
 
