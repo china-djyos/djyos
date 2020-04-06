395fd36aa82ea1e9d9774739f85b2f5ba466bee3
ck <523020590@qq.com>
2020-04-06 20:43:37 +0800

    修改了stm32的USB配置文件

--- third/usb/component_config_stusb.h
+++ third/usb/component_config_stusb.h
@@ -48,9 +48,9 @@
 //@#$%component configure   ****组件配置开始，用于 DIDE 中图形化配置界面
 //****配置块的语法和使用方法，参见源码根目录下的文件：component_config_readme.txt****
 //%$#@initcode      ****初始化代码开始，由 DIDE 删除“//”后copy到初始化文件中
-//%$#@end initcode  ****初始化代码结束
 //    extern s32 ModuleInstall_USB(const char *TargetFs,u8 controller);
 //    ModuleInstall_USB(CFG_USB_UDISK_FS, CFG_USB_CONTROLLER_ID);
+//%$#@end initcode  ****初始化代码结束
 //%$#@describe      ****组件描述开始
 //component name:"stm32usb"//ST公司的USB协议栈
 //parent:"none"                 //填写该组件的父组件名字，none表示没有父组件
