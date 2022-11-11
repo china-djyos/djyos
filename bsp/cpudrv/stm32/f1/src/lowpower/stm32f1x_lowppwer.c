7c37927f746ea54f5c12db200c42c4b45734fe7d
djyos <lst2012@yeah.net>
2022-11-11 14:29:26 +0800

    错别字“无过失责任主义”改为“无过失责任主体”

--- bsp/cpudrv/stm32/f1/src/lowpower/stm32f1x_lowppwer.c
+++ bsp/cpudrv/stm32/f1/src/lowpower/stm32f1x_lowppwer.c
@@ -36,7 +36,7 @@
 // 免责声明：本软件是本软件版权持有人以及贡献者以现状（"as is"）提供，
 // 本软件包装不负任何明示或默示之担保责任，包括但不限于就适售性以及特定目
 // 的的适用性为默示性担保。版权持有人及本软件之贡献者，无论任何条件、
-// 无论成因或任何责任主体、无论此责任为因合约关系、无过失责任主义或因非违
+// 无论成因或任何责任主体、无论此责任为因合约关系、无过失责任主体或因非违
 // 约之侵权（包括过失或其他原因等）而起，对于任何因使用本软件包装所产生的
 // 任何直接性、间接性、偶发性、特殊性、惩罚性或任何结果的损害（包括但不限
 // 于替代商品或劳务之购用、使用损失、资料损失、利益损失、业务中断等等），
@@ -184,11 +184,13 @@ void __LP_BSP_EntrySleep(u8 sleep_level, u32 pend_ticks)
         case CN_SLEEP_L0:
             //    HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFE);
             __Int_ClearAllLine();
-           HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+            HAL_PWR_EnableSEVOnPend();
+            HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFE);
             break;
         case CN_SLEEP_L1:
             __Int_ClearAllLine();
-            HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
+            HAL_PWR_EnableSEVOnPend();
+            HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFE);
             break;
         case CN_SLEEP_L2:
             //禁止中断
@@ -360,7 +362,9 @@ bool_t __LP_BSP_SaveRamL3(void)
 
 //-----------------------------------------------------------------------------
 //功能: 安装低功耗组件，要把一些低功耗需要使用到的函数，注册到系统中
-//参数: __LP_BSP_EntrySleep：进入休眠；__LP_BSP_SaveSleepLevel：保存休眠等级；__LP_BSP_SaveRamL3：保存进入休眠等级3之前的内存，
+//参数: __LP_BSP_EntrySleep：进入休眠；
+//      __LP_BSP_SaveSleepLevel：保存休眠等级；
+//      __LP_BSP_SaveRamL3：保存进入休眠等级3之前的内存，
 //      __LP_BSP_AsmSaveReg：获取含自己的返回地址在内的上下文并保存到栈中
 //返回: 无
 //-----------------------------------------------------------------------------
