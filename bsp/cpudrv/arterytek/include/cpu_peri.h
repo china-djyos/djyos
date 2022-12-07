1916cf7eeaf7bdd6998e2d244096698e1236b53f
Du Huanpeng <dhu@hodcarrier.org>
2022-12-07 19:49:09 +0800

    arm: add arterytek at32f43x initial support
    Yateli(aka. arterytek) AT32F437 series ultra-high-performance microcontrollers
    are equipped with 32-bit ARM® Cortex®-M4 core, combined with advanced technology
    and integration technology to create the industry's highest frequency performance
    of Cortex®-M4 288MHz computing speed. The built-in single-precision
    floating-point unit (FPU), digital signal processor (DSP) and memory protection unit (MPU),
    combined with rich peripherals and flexible clock control mechanism, can meet
    various application fields. It can support up to 4032KB of flash memory (Flash)
    and up to 512KB of SRAM, surpassing the level of chips of the same level in the industry.
    
    links:
    [1] https://www.arterytek.com/cn/product/AT32F435.jsp
    [2] https://www.arterytek.com/cn/product/AT32F437.jsp
    
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

---
+++ bsp/cpudrv/arterytek/include/cpu_peri.h
@@ -0,0 +1,22 @@
+#ifndef __CPU_PERI_H__
+#define __CPU_PERI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include <cpu_peri_gpio.h>
+#include <cpu_peri_int_line.h>
+#include <cpu_peri_uart.h>
+#include <get_cpuid.h>
+
+#include "at32f435_437.h"
+#include "at32f435_437_conf_template.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__CPU_PERI_H__
---
+++ third/firmware/at32f43x/component_config_at32f43x.h
@@ -0,0 +1,54 @@
+
+//@#$%component configure   ****组件配置开始，用于 DIDE 中图形化配置界面
+//****配置块的语法和使用方法，参见源码根目录下的文件：component_config_readme.txt****
+//%$#@initcode      ****初始化代码开始，由 DIDE 删除“//”后copy到初始化文件中
+//%$#@end initcode  ****初始化代码结束
+
+//%$#@describe      ****组件描述开始
+//component name:"at32f43x"//原厂固件库
+//parent:"none"                 //填写该组件的父组件名字，none表示没有父组件
+//attribute:third               //选填“third、system、bsp、user”，本属性用于在IDE中分组
+//select:choosable              //选填“required、choosable、none”，若填必选且需要配置参数，则IDE裁剪界面中默认勾取，
+                                //不可取消，必选且不需要配置参数的，或是不可选的，IDE裁剪界面中不显示，
+//init time:none                //初始化时机，可选值：early，medium，later, pre-main。
+                                //表示初始化时间，分别是早期、中期、后期
+//dependence:"none"             //该组件的依赖组件名（可以是none，表示无依赖组件），
+                                //选中该组件时，被依赖组件将强制选中，
+                                //如果依赖多个组件，则依次列出
+//weakdependence:"none"         //该组件的弱依赖组件名（可以是none，表示无依赖组件），
+                                //选中该组件时，被依赖组件不会被强制选中，
+                                //如果依赖多个组件，则依次列出，用“,”分隔
+//mutex:"none"                  //该组件的互斥组件名（可以是none，表示无互斥组件），
+                                //如果与多个组件互斥，则依次列出
+//%$#@end describe  ****组件描述结束
+
+//%$#@configue      ****参数配置开始
+#if ( CFG_MODULE_ENABLE_ART32F4 == false )
+//#warning  " stm32f4  组件参数未配置，使用默认配置"
+//%$#@target = cmdline   //header = 生成头文件,cmdline = 命令行变量，DJYOS自有模块禁用
+#define CFG_MODULE_ENABLE_ART32F4    false //如果勾选了本组件，将由DIDE在project_config.h或命令行中定义为true
+//%$#@string,1,16
+//%$#@SYMBOL        ***不经配置界面，直接定义符号
+#define AT_START_F437_V1
+#define CFG_CPU_AT32F4
+//%$#@select,1
+#define     USE_STDPERIPH_DRIVER          //RTE是否使用库
+//%$#@select,1
+//#define AT32F435CGT7   //只能选中其中一个
+//#define AT32F437ZMT7
+//%$#@free
+#endif
+//%$#@end configue  ****参数配置结束
+//%$#@exclude
+//%$#@end exclude
+
+//%$#@API include path         ****API头文件路径列表，用分号“;”隔开，以本文件所在目录为当前目录，允许用通配符，应用本库时，DIDE将自动把本列表中的头文件加到 -I 搜索路径中
+//.;.\inc;
+//..\CMSIS\Include;
+//%$#@end API include path
+
+//%$#@self include path         ****编译自身需要的头文件路径列表，用分号“;”隔开，以本文件所在目录为当前目录，允许用通配符，编译本库时，DIDE将自动把本列表中的头文件加到 -I 搜索路径中
+//%$#@end self include path
+
+//@#$%component end configure
+
