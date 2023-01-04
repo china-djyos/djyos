37a8ba2c96010513bc21ae448423c19fba63d3d0
Du Huanpeng <dhu@hodcarrier.org>
2023-01-04 19:54:02 +0800

    bsp-art43x: add example project for AT32F435CMT7
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

---
+++ examples/arterytek/src/iboot/OS_prjcfg/keepshell.c
@@ -0,0 +1,32 @@
+/****************************************************
+ *  Automatically-generated file. Do not edit!	*
+ ****************************************************/
+
+void keep_shell(void)
+{
+
+    volatile void *keep;
+    (void)keep;
+
+    extern void *djysh_heap;
+    keep = (void *)djysh_heap;
+
+    extern void *djysh_evtt;
+    keep = (void *)djysh_evtt;
+
+    extern void *djysh_catobj;
+    keep = (void *)djysh_catobj;
+
+    extern void *djysh_iapmode;
+    keep = (void *)djysh_iapmode;
+
+    extern void *djysh_reset;
+    keep = (void *)djysh_reset;
+
+    extern void *djysh_help;
+    keep = (void *)djysh_help;
+
+    extern void *djysh_time;
+    keep = (void *)djysh_time;
+
+}
